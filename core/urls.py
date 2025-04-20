68919c5cc4e45a95b97a0dc5d54c7f0f949ea17d
Anas Azamov <anasazamov55@gmail.com>
2025-04-20 16:42:56 +0500

    checked

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin-panel', admin.site.urls),
+    path('admin-panel/', admin.site.urls),
     path('', include('check_app.urls'))
 ]
