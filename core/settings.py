c8cec04c2a8152784c46ec3354436bce9f2a9976
Anas Azamov <anasazamov55@gmail.com>
2025-06-03 20:18:46 +0500

    checked

--- core/settings.py
+++ core/settings.py
@@ -121,3 +121,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://api.support-bot.uz",
+    # agar http:// ham kirsa, qo‘shing:
+    # "http://api.support-bot.uz",
+]
